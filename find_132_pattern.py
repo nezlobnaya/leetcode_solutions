c8cc3aadd2ea5dccbfadb81e60bd859b1dbd8384
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-10-25 20:04:32 -0400

    find 132 pattern solution added

---
+++ find_132_pattern.py
@@ -0,0 +1,22 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+       # 1 #left smallest
+       # 3 # mid is the largest value
+       # 2 #right is 2nd largest
+       #132 pattern
+        
+        rightNumber = float('-inf')
+        stack = []
+        for i in range( len(nums) - 1, -1, -1):
+            if nums[i] < rightNumber:
+                return True
+            else:
+                while stack and stack[-1] < nums[i]:
+                    rightNumber = stack.pop()
+                    
+            stack.append(nums[i]) 
+        return False
+
+#Time complexity O(n)       
