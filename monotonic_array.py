5b317599fa6e02d3d1f3be70e63fc06bcd6bb4b3
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-10-29 11:47:35 -0400

    minor refactoring of monotonic array

--- monotonic_array.py
+++ monotonic_array.py
@@ -7,7 +7,11 @@ An array A is monotone increasing if for all i <= j, A[i] <= A[j].  An array A i
 Return true if and only if the given array A is monotonic.
 
 """
-class Solution:
-    def isMonotonic(self, A: List[int]) -> bool:
-        return A == sorted(A) or A == sorted(A, reverse=True)
+
+def isMonotonic(A):
+    return A == sorted(A) or A == sorted(A, reverse=True)
+
+A = [1,2,2,3]
+print(isMonotonic(A))
+# Output: true
         
