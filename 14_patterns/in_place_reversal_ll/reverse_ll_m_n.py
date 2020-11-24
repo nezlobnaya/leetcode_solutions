77af34161ba3f94e27b80da75619fea9624c02ac
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-11-24 12:46:37 -0500

    minor tweaks with reverse ll from m to n

--- 14_patterns/in_place_reversal_ll/reverse_ll_m_n.py
+++ 14_patterns/in_place_reversal_ll/reverse_ll_m_n.py
@@ -3,12 +3,14 @@ Reverse a linked list from position m to n. Do it in one-pass.
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        return str(self.val) + "->" + str(self.next)
 class Solution:
-    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+    def reverseBetween(self, head, m, n):
         if not head or m == n: return head
         dummy = ListNode(0)
         dummy.next = head
@@ -24,4 +26,17 @@ class Solution:
             current.next.next, prev.next, current.next = prev.next, current.next, current.next.next
 
         return dummy.next
+
+if __name__ == "__main__":
+    l1 = ListNode(0,1)
+    l2 = ListNode(1,2)
+    l3 = ListNode(2,3)
+    l4 = ListNode(3,4)
+    l5 = ListNode(4,5)
+    l1.next = l2
+    l2.next = l3
+    l3.next = l4
+    l4.next = l5
     
+    print((f"Linked List: {l1}"))
+    print((f"Reversed LL: {Solution().reverseBetween(l1,2,4)}"))
