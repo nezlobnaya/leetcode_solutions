fba403a6561ced98563fc62614bf3f9daf424c5a
nezlobnaya <vlad.burlutskiy@icloud.com>
2020-12-18 21:42:21 -0500

    increasing triplet subsequence solution added

---
+++ increasing_triplet_sub.py
@@ -0,0 +1,24 @@
+"""
+
+Given an integer array nums, return true if there exists a triple of indices (i, j, k) 
+such that i < j < k and nums[i] < nums[j] < nums[k]. 
+If no such indices exists, return false.
+
+"""
+
+# O(n) time complexity and O(1) space complexity
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        
+        
+        num_start, middle_num = float('inf'), float('inf')
+        
+        for num in nums:
+            if num < num_start:
+                num_start = num
+            if num > num_start:
+                middle_num = min(num, middle_num)
+            if num > middle_num:
+                return True
+        return False
