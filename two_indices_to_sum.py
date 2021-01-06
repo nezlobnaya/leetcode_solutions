ca9952c4f4a3bd718de9b7a1e982bf400a5ee200
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-01-05 22:21:31 -0500

    minor tweak

--- two_indices_to_sum.py
+++ two_indices_to_sum.py
@@ -2,11 +2,28 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # hash = {}
+        # for i in range(len(nums)):
+        #     element = target - nums[i]
+        #     if element in hash:
+        #         return hash[element], i
+        #     else:
+        #         hash[nums[i]] = i
+        # return None
+ 
         hash = {}
-        for i in range(len(nums)):
-            element = target - nums[i]
+        for i,num in enumerate(nums):
+            element = target - num
             if element in hash:
                 return hash[element], i
             else:
-                hash[nums[i]] = i
+                hash[num] = i
         return None
+
+def main():
+    nums=[2,7,11,15]
+    target = 9
+
+    print(f'indices of the two numbers such that they add up to {target}: {Solution().twoSum(nums, target)}')
+
+main()
