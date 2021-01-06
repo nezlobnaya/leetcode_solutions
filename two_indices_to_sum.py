73775a5222bb6eaea95718995e10ef9cb4439306
nezlobnaya <vlad.burlutskiy@icloud.com>
2021-01-05 22:12:44 -0500

    minor tweak

--- two_indices_to_sum.py
+++ two_indices_to_sum.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Solution:
-    def twoSum(self, nums, target):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         hash = {}
         for i in range(len(nums)):
             element = target - nums[i]
