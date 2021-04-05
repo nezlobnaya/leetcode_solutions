2c94a9fe9325e55aec2e33336bb136fefcb59447
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-04-04 23:05:02 -0400

    minor refactoring of my circular queue

--- my_circular_queue.py
+++ my_circular_queue.py
@@ -19,35 +19,28 @@ class MyCircularQueue:
         
 
     def enQueue(self, value: int) -> bool:
-        if self.count==self.capacity: return False
+        if self.isFull(): return False
         self.queue[(self.headIndex + self.count) % self.capacity] = value
         self.count += 1
         return True
-        
-        
-        
+
 
     def deQueue(self) -> bool:
-        if self.count == 0:
-            return False
+         if self.isEmpty(): return False
         self.headIndex = (self.headIndex + 1) % self.capacity
         self.count -= 1
         return True
         
-        
-        
-        
+
 
     def Front(self) -> int:
-        if self.count == 0:
-            return -1
+        if self.isEmpty(): return -1
         return self.queue[self.headIndex]
        
         
 
     def Rear(self) -> int:
-        if self.count == 0:
-            return -1
+        if self.isEmpty(): return -1
         return self.queue[(self.headIndex + self.count - 1) % self.capacity]
         
         
