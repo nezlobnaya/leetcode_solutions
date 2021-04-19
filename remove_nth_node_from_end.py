8c3543cdc0fa6ce105b97bc4ea95b8035682d6e8
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2021-04-19 14:05:01 -0400

    minor refactoring

--- remove_nth_node_from_end.py
+++ remove_nth_node_from_end.py
@@ -1,8 +1,8 @@
 # two passes solution works
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:  
-        dummy = ListNode(0)
-        dummy.next = current= head
+        dummy = ListNode(0, next=head)
+        current= head
         length = 0
 
         while current:
@@ -10,7 +10,7 @@ class Solution:
             length +=1
         if length == 1: return None
         current = dummy
-        for i in range(length-n):             
+        for _ in range(length-n):             
             current = current.next
         current.next = current.next.next
         return dummy.next
